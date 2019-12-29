11d7370d430743670070a65f5d493f0b7b1c2969
Julija Trescenko <julija.trescenko@gmail.com>
2019-12-29 12:38:11 +0200

    Updates

---
+++ precache-manifest.b15a3727c8a5b08fa4627e9281a57585.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ac42ed2eec1e4d2d1a31b6f39f05fb",
+    "url": "/xy-input/index.html"
+  },
+  {
+    "revision": "d5d3fce1483631188874",
+    "url": "/xy-input/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a27bb3a3ad3b813fdbb0",
+    "url": "/xy-input/static/js/2.99fb1e75.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/xy-input/static/js/2.99fb1e75.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5d3fce1483631188874",
+    "url": "/xy-input/static/js/main.a618e210.chunk.js"
+  },
+  {
+    "revision": "8c8e02e96eb660db7cfe",
+    "url": "/xy-input/static/js/runtime-main.91409474.js"
+  }
+]);
